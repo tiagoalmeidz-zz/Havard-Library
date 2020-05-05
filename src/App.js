49e5f3b662b80a013d893bdb2ff32d36881d9612
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-05-04 23:51:05 -0300

    Criando component Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Routes from "./routes";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
